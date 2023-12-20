7f991033502d915526acea2aa14cd0e5f5dff4e0
Smruti-Rani <91662340+Smrutirani@users.noreply.github.com>
2023-12-20 10:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student = {
+	name: "Smruti"
+}
+Objects.prototype.getKeys = function() {
+	return Ojects.keys(this);
+}
+const keys = this.student();
+console.log(keys);
